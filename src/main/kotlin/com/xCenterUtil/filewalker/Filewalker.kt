fef6053aff74990bdd21d8c67e6059fa1ff51f52
Pat_Looft <plooft@metlife.org>
2022-07-21 08:35:30 -0400

    got the folders with xCenters printing out

--- src/main/kotlin/com/xCenterUtil/filewalker/Filewalker.kt
+++ src/main/kotlin/com/xCenterUtil/filewalker/Filewalker.kt
@@ -6,10 +6,11 @@ class Filewalker {
     //Companion object is used for class functions as far as I can see it
     companion object Walker{//, Texas Ranger
         val pathToStart = "c://"
+        var currentPath = pathToStart;
         //function walkPWDForXCenters()
         //checks all folders in current working directory and returns all that have Center in the name
         //this will need to check in every folder in current location, then check folder names in there
-        fun retrieveFileListOfCurrentPath() : List<File>{
+        fun retrieveFileListOfCurrentPath() : Array<File>{
             //TODO: exception handling for file reading
             var curFolder = File(pathToStart)
             /*        var curFolder : File
@@ -30,11 +31,37 @@ class Filewalker {
                 println(it.name)
             }*/
             println("PATH: ${curFolder.absoluteFile}")
-            println("SIZE: ${curFolder.listFiles().forEach { println("Current: ${it.name}") }}")
+            curFolder.listFiles().forEach { println("Current: ${it.name}") }
 
             //return a list of Files in current folder
-//            return curFileTreeWalk.toList()
-            return curFolder.walk().toList()
+            return curFolder.listFiles()
+        }
+
+        //not sure if this should return the files themselves or just an array of paths as Strings
+        fun checkFolderForxCenters(currentFolder : File) : MutableList<String> {
+            //list to store xCenter paths as strings
+            val xCenterList = mutableListOf<String>()
+
+            //all files in current folder
+            val currentFolderContents = currentFolder.listFiles()
+
+            println("Currently checking following path for xCenters: \'${currentFolder.absolutePath}\'")
+
+            if(currentFolderContents != null){
+                //iterate through current folders child files and see if any of them have "Center" in their name
+                currentFolderContents.forEach {
+                    var fileName = it.name
+                    if(fileName.contains("Center") || fileName.contains("cc")
+                        || fileName.contains("pc") || fileName.contains("cm")){
+                        println("\txCenter found at: ${fileName}")
+                        xCenterList.add(it.absolutePath)
+                    }
+                }
+            }
+            else{
+                println("Skipping ${currentFolder.path}")
+            }
+            return xCenterList
         }
     }
 }
--- src/main/kotlin/com/xCenterUtil/filewalker/FilewalkerApplication.kt
+++ src/main/kotlin/com/xCenterUtil/filewalker/FilewalkerApplication.kt
@@ -2,13 +2,32 @@ package com.xCenterUtil.filewalker
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.io.File
 
 @SpringBootApplication
 class FilewalkerApplication
 
 fun main(args: Array<String>) {
 	runApplication<FilewalkerApplication>(*args)
-	println("pre call")
-	Filewalker.retrieveFileListOfCurrentPath()
-	println("post call")
+	var filePathResultList = mutableListOf<String>()
+
+	//grab all files in the current folder
+	println("pre retrieveFileListOfCurrentPath")
+	val filesInCurrentPath = Filewalker.retrieveFileListOfCurrentPath()
+	println("post retrieveFileListOfCurrentPath")
+
+	println("pre retrieveFileListOfCurrentPath")
+	filesInCurrentPath.forEach {
+		var result = Filewalker.checkFolderForxCenters(it)
+		result.forEach{
+			filePathResultList.add(it)
+		}
+//		println("${result.size} xCenters found for path ${Filewalker.currentPath}")
+	}
+	println("post retrieveFileListOfCurrentPath")
+
+	println("Paths with xCenters:")
+	filePathResultList.forEach{
+		println("\t${it}")
+	}
 }
