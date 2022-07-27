9fdf33af6e1f9cbd0627be7b877dea0767564e65
Pat_Looft <plooft@metlife.org>
2022-07-27 11:20:42 -0400

    Got Path.fetchPath() returning without errors. It is returning a Future<Path> instead of a path, so I will be continuing to figure out how to get the Path out of the Future<Path>. This method gets back a singular path and id and returns the Future<Path> as is

---
+++ frontend/lib/Path.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
+class Path{
+  String id = "";
+  String text = "";
+
+  Path({
+    required this.id,
+    required this.text,
+  });
+
+  Path.fromJson(Map<String, dynamic> json){
+      id = json['id'];
+      text = json['text'];
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//                          Get - single path                                 //
+////////////////////////////////////////////////////////////////////////////////
+
+Future<Path> fetchPath() async {
+  var url = Uri.parse("http://localhost:8080/1");
+  final response = await http.get(url);
+  Map<String, dynamic> decodedResponseBody = json.decode(response.body);
+
+
+  if(response.statusCode == 200){
+    var responseBody = response.body;
+    print("200 ok - $responseBody");
+    Path returnVal = Path(
+        id: decodedResponseBody['id'],
+        text: decodedResponseBody['text']
+    );
+
+    return returnVal;
+  }
+  else{
+    throw Exception("Failed to retrieve paths");
+  }
+}
+
--- src/main/kotlin/com/xCenterUtil/filewalker/FileWalkerController.kt
+++ src/main/kotlin/com/xCenterUtil/filewalker/FileWalkerController.kt
@@ -1,6 +1,7 @@
 package com.xCenterUtil.filewalker
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 class FileWalkerController {
@@ -11,6 +12,32 @@ data class Message(val id: String?, val text: String)
 @RestController
 class MessageResource{
     var idCounter : Int = 0;
+
+    //trying to get this working with a map
+/*    @GetMapping("/")
+    fun getXCenterPaths() : MutableMap<String, Message> {
+//        var filePathResultList = mutableListOf<Message>()
+        var resultMap = mutableMapOf<String, Message>()
+
+        //grab all files in the current folder
+        println("pre retrieveFileListOfCurrentPath")
+        val filesInCurrentPath = Filewalker.retrieveFileListOfCurrentPath()
+        println("post retrieveFileListOfCurrentPath")
+
+        println("pre retrieveFileListOfCurrentPath")
+        filesInCurrentPath.forEach {
+            var result = Filewalker.checkFolderForxCenters(it)
+            result.forEach{
+                val currentPathMessage = Message(idCounter++.toString(), it)
+//                filePathResultList.add(currentPathMessage)
+                print("Adding \'${it}\' to the map")
+                resultMap[idCounter++.toString()] = currentPathMessage
+            }
+        }
+        println("post retrieveFileListOfCurrentPath")
+
+        return resultMap;
+    }*/
 
     @GetMapping("/")
     fun getXCenterPaths() : List<Message> {
@@ -32,5 +59,30 @@ class MessageResource{
         println("post retrieveFileListOfCurrentPath")
 
         return filePathResultList;
+    }
+
+    @GetMapping("/{id}")
+    fun getXCenterPathById(@PathVariable id : Int) : Message {
+        var filePathResultList = mutableListOf<Message>()
+
+        //grab all files in the current folder
+        println("pre retrieveFileListOfCurrentPath")
+        val filesInCurrentPath = Filewalker.retrieveFileListOfCurrentPath()
+        println("post retrieveFileListOfCurrentPath")
+
+        println("pre retrieveFileListOfCurrentPath")
+        filesInCurrentPath.forEach {
+            var result = Filewalker.checkFolderForxCenters(it)
+            result.forEach{
+                val currentPathMessage = Message(idCounter++.toString(), it)
+                filePathResultList.add(currentPathMessage)
+            }
+        }
+        ;
+        println("post retrieveFileListOfCurrentPath")
+
+        println("Returning message with ID = ${id} and content: ${filePathResultList.get(id).text}")
+
+        return filePathResultList.get(id)
     }
 }
