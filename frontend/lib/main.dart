85461dc53663de7c9bc0bffaaeeb0c129a54ca99
Pat_Looft <plooft@metlife.org>
2022-08-01 13:54:30 -0400

    finally got the text out of the future object on the front end. I am able to print the path I am grabbing. Next will be cleaning this file up as it has lots of comments describing what things do, as well as some unused code

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -32,7 +32,8 @@ class MyApp extends StatelessWidget {
           title:const Text('xCenter path finder')
         ),
         body: Center(
-          child: XCenterPaths(),
+          // child: XCenterPaths(),
+          child: MyHomePage(title: 'Help',),
         )
       )//const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -96,38 +97,10 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        // child: Column(
-        /*child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-
-          ],
-        ),*/
+        // Center is a layout widget. It takes a single child and positions it in the middle of the parent.
+        // child: Column( //was here
           child: FutureBuilder<Path>(
-              future: futurePath,
+              future: fetchPath(),
               builder: (context, snapshot){
                 if(snapshot.hasData){
                   print("has data ${snapshot.data!},\n${snapshot.data!.text}");
@@ -163,11 +136,6 @@ class XCenterPaths extends StatefulWidget {
 class _XCenterPathsState extends State<XCenterPaths> {
   @override
   Widget build(BuildContext context) {
-    // var url = Uri.parse("http://localhost:8080/");
-    // final response = http.get(url);
-    // if(response.st)
-    // print("response ${response.toString()}");
-    // response.
     Future<Path> response = fetchPath();
     var responseString = response.toString();
     print("RESPONSE STRING: $responseString");
